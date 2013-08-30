79bb03f72cf7120f2f555bf0d1efe07e1aa8c01f
cliffordp <tko@tourkick.com>
2013-08-29 19:26:53 -0500

    wrapped in a plugin

---
+++ bigtext.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: BigText PL Section
+Author: TourKick
+Author URI: http://tourkick.com/?utm_source=pagelines&utm_medium=section&utm_content=authoruri&utm_campaign=bigtext_section
+Plugin URI: http://www.pagelinestheme.com/bigtext-section?utm_source=pagelines&utm_medium=section&utm_content=pluginuri&utm_campaign=bigtext_section
+Description: A <a href="https://github.com/zachleat/BigText" target="_blank">BigText</a> section that resizes text to fit one or more words on a line that fits the container, and is responsive which means it scales with different size browsers. Like <a href="www.pagelines.com/store/sections/fittext-section/" target="_blank">FitText</a> but more customizable!
+Demo: http://www.pagelinestheme.com/bigtext-section?utm_source=pagelines&utm_medium=section&utm_content=demolink&utm_campaign=bigtext_section
+Class Name: BigTextSection
+Version: 2.20130829
+*/
